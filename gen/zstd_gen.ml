b54a2eb7a33cc6e1288f4953760bb720e615e754
Xavier Clerc <xclerc@janestreet.com>
2020-06-24 09:56:36 +0100

    v0.15-preview.123.04+85

--- gen/zstd_gen.ml
+++ gen/zstd_gen.ml
@@ -5,7 +5,11 @@ let () =
   (* This allows us to expose the various dictionary training functions   *)
   Format.fprintf fmt_c {|#define ZDICT_STATIC_LINKING_ONLY@.|};
   Format.fprintf fmt_c {|#include "zdict.h"@.|};
-  Cstubs.write_c fmt_c ~prefix:"caml_" (module Zstd_bindings.C);
+  Cstubs.write_c
+    ~concurrency:Cstubs.unlocked
+    fmt_c
+    ~prefix:"caml_"
+    (module Zstd_bindings.C);
   let fmt_ml = fmt "zstd_generated.ml" in
   Cstubs.write_ml fmt_ml ~prefix:"caml_" (module Zstd_bindings.C);
   flush_all ()
